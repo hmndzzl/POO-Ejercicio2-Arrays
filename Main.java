8c7ec7d3acf9415d8f32914570a0bc353659aa74
hmndzzz <hrmendezlee@gmail.com>
2024-08-23 11:27:26 -0600

    agregar encabezado

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+//
+// Programación orientada a objetos
+// Sección 20
+// Ejercicio 1
+// 
+// Hugo Méndez - 241265
+// Diego Calderón = 241263
+//
+
 public class Main{
     public static void main(String[] args) {
         
