b8408f66873e720d650b873015c227ee2ae101b8
Diego-glitch-cloud <dcalderon918@gmail.com>
2024-08-23 11:13:13 -0600

    Main con To Do

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
-public class Main{
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        
+        // Inicializar el sistema e ingresar los datos de 4 salones
+        for (int i = 0; i < 4; i++) {
+            // Solicito el nombre del jugador al usuario
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Ingrese el nombre del salón %i: ", i + 1);
+            String nombreSalon = scanner.nextLine();
+
+            int numSalon = 0;
+            do {
+                if (numSalon < 0) {
+                    System.out.println("Debe ingresar un valor entero positivo, intente de nuevo.");
+                }
+                System.out.print("Ingrese el número de identificación del salón %i: ", i + 1);
+                numSalon = scanner.nextInt();
+            } while (numSalon < 0);
+
+        }
+        // Realizar un menú
+        // Recibir una solicitud de reserva
+        // Asignar un salón a un evento
+        // Implementar lógica para gestionar la lista de espera
+        // Implementar lófica para verificar conlfictos de horario
+        // Generar reportes de eventos
     }
 }
