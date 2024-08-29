0fad3dff2daf0dffde4fbc22a2b80d7b08f17e01
Diego-glitch-cloud <dcalderon918@gmail.com>
2024-08-28 23:44:57 -0600

    Estructura principal del menú

--- Main.java
+++ Main.java
@@ -1,77 +1,129 @@
 import java.util.Scanner;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        // Inicializar el sistema e ingresar los datos de 4 salones
-        Salon[] salones = new Salon[4];
+        // // Inicializar el sistema e ingresar los datos de 4 salones
+        // Salon[] salones = new Salon[4];
 
-        boolean invalido = true;
-        String tipoSalon = "";
-        String nombreSalon;
-        int capSalon;
-        int costoSalon;
+        // boolean invalido = true;
+        // String tipoSalon = "";
+        // String nombreSalon;
+        // int capSalon;
+        // int costoSalon;
 
-        // Crear una instancia de la clase Scanner
-        Scanner scanner = new Scanner(System.in);
+        // // Crear una instancia de la clase Scanner
+        // Scanner scanner1 = new Scanner(System.in);
 
-        for (int i = 1; i < 5; i++) {
-            // Pedir el nombre del salón al usuario
-            System.out.printf("Ingrese el nombre del salón %d: ", i);
-            nombreSalon = scanner.nextLine();
+        // // Mensaje para los empleados del centro (usuarios del sistema)
+        // System.out.println("A continuación deberá ingresar los datos de 4 salones
+        // disponibles para ser reservados: ");
 
-            // Pedir la capacidad del salón al usuario
-            do {
-                System.out.printf("Ingrese la capacidad máxima del salón %d: ", i);
-                capSalon = scanner.nextInt();
-                scanner.nextLine(); // Consumir el salto de línea restante
+        // for (int i = 1; i < 5; i++) {
+        // // Pedir el nombre del salón al usuario
+        // System.out.printf("Ingrese el nombre del salón %d: ", i);
+        // nombreSalon = scanner1.nextLine();
 
-                if (capSalon >= 500) {
-                    tipoSalon = "Grande";
-                } else if (capSalon < 500 && capSalon > 200) {
-                    tipoSalon = "Mediano";
-                } else if (capSalon <= 200 && capSalon > 0) {
-                    tipoSalon = "Pequeño";
-                } else {
-                    System.out.println("Capacidad inválida. Vuelva a intentarlo.");
-                    continue;
-                }
+        // // Pedir la capacidad del salón al usuario
+        // do {
+        // System.out.printf("Ingrese la capacidad máxima del salón %d: ", i);
+        // capSalon = scanner1.nextInt();
+        // scanner1.nextLine(); // Consumir el salto de línea restante
 
-                invalido = false;
-            } while (invalido);
+        // if (capSalon >= 500) {
+        // tipoSalon = "Grande";
+        // } else if (capSalon < 500 && capSalon > 200) {
+        // tipoSalon = "Mediano";
+        // } else if (capSalon <= 200 && capSalon > 0) {
+        // tipoSalon = "Pequeño";
+        // } else {
+        // System.out.println("Capacidad inválida. Vuelva a intentarlo.");
+        // continue;
+        // }
 
-            invalido = true;
-            do {
-                System.out.printf("Ingrese el costo por hora del salón %d: ", i);
-                costoSalon = scanner.nextInt();
-                scanner.nextLine(); // Consumir el salto de línea restante
+        // invalido = false;
+        // } while (invalido);
 
-                if (costoSalon < 0) {
-                    System.out.println("Costo inválido. Ingrese un valor positivo.");
-                    continue;
-                }
+        // invalido = true;
+        // do {
+        // System.out.printf("Ingrese el costo por hora del salón %d: ", i);
+        // costoSalon = scanner1.nextInt();
+        // scanner1.nextLine(); // Consumir el salto de línea restante
 
-                invalido = false;
-            } while (invalido);
+        // if (costoSalon < 0) {
+        // System.out.println("Costo inválido. Ingrese un valor positivo.");
+        // continue;
+        // }
 
-            // Agregar los salones al Array
-            salones[i - 1] = new Salon(nombreSalon, tipoSalon, capSalon, costoSalon); // El número de salón es su index
-        }
+        // invalido = false;
+        // } while (invalido);
 
-        // Cerrar el scanner
-        scanner.close();
+        // // Agregar los salones al Array
+        // salones[i - 1] = new Salon(nombreSalon, tipoSalon, capSalon, costoSalon); //
+        // El número de salón es su index
+        // }
 
-        System.out.println("Length de salones: " + salones.length);
+        // // Cerrar el scanner
+        // scanner1.close();
 
-        // Imprimir los salones
-        for (int j = 0; j < salones.length; j++) {
-            System.out.printf("Nombre: %s | Tipo: %s | Capacidad: %d | Costo: %d | Número: %d\n",
-                    salones[j].getNombre(), salones[j].getTipo(), salones[j].getCapacidad(), salones[j].getCosto(), j);
-        }
+        // //
+        // ---------------------------------------------MENÚ------------------------------------------------
+
+        // // Imprimir los salones
+        // for (int j = 0; j < salones.length; j++) {
+        // System.out.printf("Nombre: %s | Tipo: %s | Capacidad: %d | Costo: %d |
+        // Número: %d\n",
+        // salones[j].getNombre(), salones[j].getTipo(), salones[j].getCapacidad(),
+        // salones[j].getCosto(), j);
+        // }
+
+        // Realizar un menú
+        boolean wmenu = true;
+
+        String menu = """
+                    Ingrese el número correspondiente al valor de la opción que desea ejecutar:
+
+                    1) Recibir solicitud de reserva.
+                    2) Imprimir información de Salones.
+                    3) Asignar salón a un evento.
+                    4) Generar reportes de eventos.
+                    5) Salir.
+                """;
+
+        Scanner scanner2 = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println(menu);
+            System.out.print("Opción: ");
+
+            opcion = scanner2.nextInt();
+            scanner2.nextLine();
+
+            // Verificar que el usuario ingrese una opción dentro de las posibles.
+            if (opcion < 1 || opcion > 5) {
+                System.out.println("Opción incorrecta. Ingrese un número entre 1 y 5.");
+                continue;
+            }
+
+            // Recibir solicitud de reserva
+            if (opcion == 1) {
+
+            }
+
+            // Salir
+            if (opcion == 5) {
+                System.out.println("Saliendo...");
+                wmenu = false;
+            }
+
+        } while (wmenu == true);
+
+        scanner2.close();
+
+        // Recibir una solicitud de reserva
+        // Asignar un salón a un evento
+        // Implementar lógica para gestionar la lista de espera
+        // Implementar lófica para verificar conlfictos de horario
+        // Generar reportes de eventos
     }
-    // Realizar un menú
-    // Recibir una solicitud de reserva
-    // Asignar un salón a un evento
-    // Implementar lógica para gestionar la lista de espera
-    // Implementar lófica para verificar conlfictos de horario
-    // Generar reportes de eventos
 }
